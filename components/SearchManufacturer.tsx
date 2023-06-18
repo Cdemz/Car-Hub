e1388a4706c0802cd0869639b58ceac0df067999
cdemz <gbengaadepoju75@gmail.com>
2023-06-18 09:33:57 +0100

    i snoozed

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,30 @@
+"use client";
+import { Combobox, Transition } from "@headlessui/react";
+import { SearchManufacturerprops } from "@/types";
 import React from "react";
+import Image from "next/image";
 
-const SearchManufacturer = ({ manufacturer, setManufacturer }) => {
-  return <div>SearchManufacturer</div>;
+const SearchManufacturer = ({
+  manufacturer,
+  setManufacturer,
+}: SearchManufacturerprops) => {
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              alt=""
+              height={20}
+              className="ml-4"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
